723a34de61378817b3fe9ce97c41bd8b22e6b1ed
Gao Yuan <gy001@foxmail.com>
2022-11-14 20:28:05 +0800

    Create yimiao.js

---
+++ yimiao.js
@@ -0,0 +1,67 @@
+{
+  "success" : true,
+  "data" : [
+    {
+      "id" : 393577847,
+      "serverUpdateTime" : "2022-11-14 01:33:35",
+      "phone" : "176****1887",
+      "vaccineCount" : 4,
+      "birth" : "20**-**-19",
+      "vaccineFactoryCode" : "EF",
+      "address" : "山东省.日照市.东港区.石臼******",
+      "fchildNo" : "371102200101191651",
+      "batchNum" : "202107082O",
+      "lastStationName" : "石臼街道林海社区卫生服务站预防接种门诊",
+      "lastJzrq" : "2022-10-13 08:47:54",
+      "vaccineCode" : "G4",
+      "name" : "*源"
+    },
+    {
+      "id" : 393477847,
+      "serverUpdateTime" : "2022-11-14 01:33:35",
+      "phone" : "176****1887",
+      "vaccineCount" : 3,
+      "birth" : "20**-**-19",
+      "vaccineFactoryCode" : "EF",
+      "address" : "山东省.日照市.东港区.石臼******",
+      "fchildNo" : "371102200101191651",
+      "batchNum" : "202107082O",
+      "lastStationName" : "石臼街道林海社区卫生服务站预防接种门诊",
+      "lastJzrq" : "2022-08-19 18:47:54",
+      "vaccineCode" : "G3",
+      "name" : "*源"
+    },
+    {
+      "id" : 393377847,
+      "serverUpdateTime" : "2022-11-14 01:33:35",
+      "phone" : "176****1887",
+      "vaccineCount" : 2,
+      "birth" : "20**-**-19",
+      "vaccineFactoryCode" : "EF",
+      "address" : "山东省.日照市.东港区.石臼******",
+      "fchildNo" : "371102200101191651",
+      "batchNum" : "202107082O",
+      "lastStationName" : "石臼街道林海社区卫生服务站预防接种门诊",
+      "lastJzrq" : "2021-08-13 08:17:54",
+      "vaccineCode" : "G2",
+      "name" : "*源"
+    },
+    {
+      "id" : 393369840,
+      "serverUpdateTime" : "2022-11-14 01:33:35",
+      "phone" : "176****1887",
+      "vaccineCount" : 1,
+      "birth" : "20**-**-19",
+      "vaccineFactoryCode" : "EF",
+      "address" : "山东省.日照市.东港区.石臼******",
+      "fchildNo" : "371102200101191651",
+      "batchNum" : "202106095G",
+      "lastStationName" : "石臼街道林海社区卫生服务站预防接种门诊",
+      "lastJzrq" : "2021-07-21 08:28:19",
+      "vaccineCode" : "G1",
+      "name" : "*源"
+    }
+  ],
+  "code" : 0,
+  "msg" : null
+}
