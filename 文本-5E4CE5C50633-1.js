e6091a8e7ede5969eac31de0c6326411ebd27284
Gao Yuan <gy001@foxmail.com>
2022-11-15 10:19:18 +0800

    Create 文本-5E4CE5C50633-1.js

---
+++ 文本-5E4CE5C50633-1.js
@@ -0,0 +1,19 @@
+{
+  "status" : 0,
+  "success" : true,
+  "content" : [
+    {
+      "boo" : true,
+      "ssoLogin" : null,
+      "errorCode" : null,
+      "productId" : 63136,
+      "productStock" :424,
+      "productBranchStock" : 1,
+      "stockMode" : "1",
+      "buttonLight" : "1",
+      "buttonWord" : "已领取11",
+      "orderTips" : "下单次数不能超过限制11"
+    }
+  ],
+  "msg" : null
+}
